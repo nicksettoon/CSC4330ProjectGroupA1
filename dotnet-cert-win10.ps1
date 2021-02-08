d50f036239860a1e96a4c06694b73ff88abfab14
nicksettoon <nick+git@settoon.info>
2021-02-08 02:33:15 -0600

    trying to get dev docker-compose working

--- dotnet-cert-win10.ps1
+++ dotnet-cert-win10.ps1
@@ -1,1 +1,5 @@
 # powershell script for handling self-signed dev certificates needed for the containers
+
+$CERT_PSWD = [guid]::NewGuid()
+dotnet dev-certs https -ep %USERPROFILE%/.aspnet/https/Backend.pfx -p CERT_PSWD
+dotnet dev-certs https --trust
