8a6e14a9ffc232d26c01acb9a3c6f20e4a7d996b
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-02-07 23:44:32 -0600

    - Added needed files for Rent Feature and started implementing Rent.
    :p

---
+++ Entities/Bike.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSC4330ProjectGroupA1.Entities
+{
+    public class Bike
+    {
+        public int BikeId { get; set; }
+
+        public bool Rented { get; set; }
+    }
+}
---
+++ Models/RentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSC4330ProjectGroupA1.Models
+{
+    public class RentModel
+    {
+        public int BikeNumber { get; set; }
+    }
+}
