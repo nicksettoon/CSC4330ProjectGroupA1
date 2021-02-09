f080b7315b789cf0fde637829d5d489ed9bb96fc
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-02-08 21:51:22 -0600

    added Identity wire up

---
+++ Backend/Areas/Identity/Data/BackendUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Backend.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the BackendUser class
+    public class BackendUser : IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; }
+
+        //public string Email { get; set; }
+
+        [PersonalData]
+        public string Password { get; set; }
+
+        [PersonalData]
+        public CreditCard CreditCard { get; set; }
+
+    }
+}
