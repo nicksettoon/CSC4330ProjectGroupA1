f79c36dcf3d7678a41b1fef3ff4f2601e14a70a9
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-02-10 21:29:30 -0600

    - more identity work

--- Backend/Areas/Identity/Data/BackendUser.cs
+++ Backend/Areas/Identity/Data/BackendUser.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Backend.Entities;
 using Microsoft.AspNetCore.Identity;
 
 namespace Backend.Areas.Identity.Data
@@ -10,16 +9,5 @@ namespace Backend.Areas.Identity.Data
     // Add profile data for application users by adding properties to the BackendUser class
     public class BackendUser : IdentityUser
     {
-        [PersonalData]
-        public string Name { get; set; }
-
-        //public string Email { get; set; }
-
-        [PersonalData]
-        public string Password { get; set; }
-
-        [PersonalData]
-        public CreditCard CreditCard { get; set; }
-
     }
 }
