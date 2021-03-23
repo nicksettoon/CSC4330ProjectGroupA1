c3b4e34ce901951e049ae3cdfcaabe7566059587
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-03-22 20:31:02 -0500

    - Added email checking in Rentals

--- Backend/Controllers/RentController.cs
+++ Backend/Controllers/RentController.cs
@@ -7,6 +7,7 @@ using Backend.Models;
 using Backend.Context;
 using Backend.Entities;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace DowlingBikes
 {
@@ -24,9 +25,14 @@ namespace DowlingBikes
             {
                 data.CheckOut = System.DateTime.Now;
 
+                /*ClaimsPrincipal currentUser = this.User;
+                var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;*/
+
+                var userEmail = User.Identity.Name;
+
                 using (var context = new DowlingContext())
                 {
-                    context.Rentals.Add(new Rental() { CheckOutTime = data.CheckOut, BikeNumber = data.BikeNumber, RenterEmail = "FatRat"});
+                    context.Rentals.Add(new Rental() { CheckOutTime = data.CheckOut, BikeNumber = data.BikeNumber, RenterEmail = userEmail });
                     context.SaveChanges();
                 }
 
@@ -37,10 +43,15 @@ namespace DowlingBikes
             {
                 data.CheckIn = DateTime.Now;
 
+                /*ClaimsPrincipal currentUser = this.User;
+                var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;*/
+
+                var userEmail = User.Identity.Name;
+
                 using (var context = new DowlingContext())
                 {
                     var rental = from a in context.Rentals
-                                 where a.BikeNumber.Equals(data.BikeNumber) && a.CheckInTime.Equals(new DateTime())
+                                 where a.BikeNumber.Equals(data.BikeNumber) && a.CheckInTime.Equals(new DateTime()) && a.RenterEmail.Equals(userEmail)
                                  select a;
 
                     context.Entry(rental.First()).Property(u => u.CheckInTime).CurrentValue = data.CheckIn;
