651b7691afb25b9f4d6b8d10845724005210e7f5
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-03-25 18:57:48 -0500

    - Fixed issues with login

--- Backend/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Backend/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,13 +12,13 @@ namespace Backend.Areas.Identity.Pages.Account.Manage
 {
     public class ChangePasswordModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<BackendUser> _userManager;
+        private readonly SignInManager<BackendUser> _signInManager;
         private readonly ILogger<ChangePasswordModel> _logger;
 
         public ChangePasswordModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<BackendUser> userManager,
+            SignInManager<BackendUser> signInManager,
             ILogger<ChangePasswordModel> logger)
         {
             _userManager = userManager;
--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Areas.Identity.Data;
+using Backend.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +28,8 @@ namespace Backend
         {
             services.AddRazorPages();
             services.AddControllersWithViews();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
