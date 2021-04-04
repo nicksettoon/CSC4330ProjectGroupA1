aa485d1965f49cc802dffecdfd9f111408be85f5
nicksettoon <nick+git@settoon.info>
2021-04-04 17:02:42 -0500

    docker changes for windows

--- dotnet-cert-win10.ps1
+++ dotnet-cert-win10.ps1
@@ -3,3 +3,5 @@
 $CERT_PSWD = [guid]::NewGuid()
 dotnet dev-certs https -ep %USERPROFILE%/.aspnet/https/Backend.pfx -p CERT_PSWD
 dotnet dev-certs https --trust
+
+docker-compose --profile dev --profile support up --build
