b953a7f6689838622254228d0c6d1d48e76c2529
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-04-05 12:05:52 -0500

    - Added model, report it missing feature, added database access for reports
    ferris is a rat

--- Backend/Controllers/AdminController.cs
+++ Backend/Controllers/AdminController.cs
@@ -8,14 +8,17 @@ using Microsoft.Extensions.Logging;
 using Backend.Models;
 using Microsoft.AspNetCore.Authorization;
 using Backend.Context;
+using Backend.Entities;
 
 namespace Backend.Controllers
 {
-    
+
 
     [Authorize]
     public class AdminController : Controller
     {
+        private const double MissingBikeCharge = 500.0;
+
         private readonly ILogger<AdminController> _logger;
 
         public AdminController(ILogger<AdminController> logger)
@@ -25,19 +28,30 @@ namespace Backend.Controllers
 
         public IActionResult Index(Areas.Identity.Data.BackendUser profile)
         {
-            ViewData["USER"] = profile.Name;
+            ViewData["USER"] = User.Identity.Name;
             return View();
         }
 
         public IActionResult NewReport()
         {
-            /*using (var context = new DowlingContext())
+            var data = new AdminModel();
+
+            var userEmail = User.Identity.Name;
+
+            data.CurrentUserEmail = userEmail;
+
+            using (var context = new DowlingContext())
             {
-                var rental = from a in context.Rentals
-                             where a.CheckInTime.Equals()
-                             select a;
-            }*/
-                return View();
+                var monday = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - 6);
+
+                var rentalQuery = from a in context.Rentals
+                                  where a.CheckOutTime.CompareTo(monday) >= 0 && a.CheckOutTime.CompareTo(DateTime.Now.AddDays(1)) <= 0
+                                  select a;
+                var rentals = rentalQuery.ToList();
+                data.Rentals = rentals;
+            }
+
+            return View("NewReport", data);
         }
 
         public IActionResult OldReport()
@@ -49,6 +63,31 @@ namespace Backend.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult MissingBike(string id)
+        {
+            var bikeNumber = Int32.Parse(id);
+            using (var context = new DowlingContext())
+            {
+                var rentalQuery = from a in context.Rentals
+                                  where a.BikeNumber.Equals(bikeNumber) && a.ReturnDock == 0
+                                  select a;
+
+                if (rentalQuery.Any())
+                {
+                    var entry = context.Entry(rentalQuery.First());
+
+                    entry.Property(u => u.CheckInTime).CurrentValue = DateTime.Now;
+
+                    entry.Property(u => u.Price).CurrentValue += MissingBikeCharge;
+
+                    entry.Property(u => u.ReturnDock).CurrentValue = entry.Property(u => u.RentDock).CurrentValue;
+                }
+
+                context.SaveChanges();
+            }
+                return View("Index");
         }
     }
 }
---
+++ Backend/Models/AdminModel.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class AdminModel
+    {
+        public IList<Rental> Rentals { get; set; }
+
+        public string CurrentUserEmail { get; set; }
+    }
+}
