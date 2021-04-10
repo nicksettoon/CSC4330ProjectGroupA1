6b59836e22bc81eceb7849ba2795f7e026ee0431
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-04-10 11:36:14 -0500

    - Added data access for dock report

---
+++ Backend/Models/DockModel.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class DockModel
+    {
+        public int DockNumber { get; set; }
+
+        public List<Rental> Rentals { get; set; }
+
+        public double TotalRevenue { get; set; }
+    }
+}
